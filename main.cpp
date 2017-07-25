61aeb51844994b607a89689d72a0c71a5bb2b55f
Warlock-Dalbaeb <a.yvarov@yandex.ru>
2017-07-25 21:21:27 +0700

    .

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 //#include <map>
 //#include <vector>
+#include <math.h> 
 
 namespace HelloWorldName{
 
@@ -40,6 +41,45 @@ HelloWorldName ::  HelloWorld :: ~HelloWorld (void){
  std::cout << "\n\nRun delete\n";
 } 
 
+namespace size_of{
+template <typename Type> int size_of(const char * name){
+ std::cout << "Size of " << name << " == " << sizeof(Type) << std::endl;
+ return sizeof(Type);
+}
+
+}
+
+namespace chars{
+void from_to_write(int from,int to){
+for(int i = from;i <= to;i++) std::cout << (char)i << " 0x" << std::hex << i << std::endl;
+}
+}
+
+namespace limits{
+template <typename Type> int min_value_of_type(const char*name,bool sig){
+if ( sig ){
+std::cout << "max value of " << name << " " << (pow(2, (sizeof(Type)*8) ) / 2 - 1)*-1 <<std::endl;
+}else{
+return 0;
+}
+
+}
+template <typename Type> void max_value_of_type(const char*name,bool sig){
+
+if ( sig ){
+std::cout << "max value of " << name << " " << pow(2, (sizeof(Type)*8) ) / 2 - 1 <<std::endl;
+}else{
+std::cout << "max value of " << name << " " << pow(2, (sizeof(Type)*8) ) <<std::endl;
+}
+
+
+//return pow(2, (sizeof(Type)*8) );
+}
+
+
+}
+
+
 int main(void){
 //std::cout << "Create object" << std::endl;
 HelloWorldName::HelloWorld * HelloWorldObject = new HelloWorldName::HelloWorld();
@@ -49,4 +89,10 @@ HelloWorldObject->GiveName(teststring);
 //HelloWorldObject->Run();
 delete HelloWorldObject;
 //std::cout << "Object created" << std::endl;
+size_of::size_of<char>("char");
+size_of::size_of<int>("int");
+size_of::size_of<long long>("long long");
+chars::from_to_write(48,57);
+limits::min_value_of_type<char>("char",true);
+limits::max_value_of_type<char>("char",true);
 }
